f09e156934edb2069b3b5f575d33607a24f5be7d
Henry <henrywlam+github@gmail.com>
2022-11-23 21:19:53 -0500

    updated price results

--- scraper.py
+++ scraper.py
@@ -50,7 +50,7 @@ def get_doseandqty(url, drugname, zip, form):
     filters = driver.find_elements(By.CLASS_NAME, 'ant-select-selection-item')
     filters[0].click()
     form_selection = driver.find_element(By.XPATH , f'//div[@class="ant-select-item-option-content" and text()="{form}"]')
-    WebDriverWait(driver, timeout=5).until(EC.element_to_be_clickable(form_selection))
+    WebDriverWait(driver, timeout=5).until(EC.visibility_of(form_selection))
     form_selection.click()
     page = WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CLASS_NAME,'c-results__item')))
     for menu in filters:
@@ -63,12 +63,48 @@ def get_doseandqty(url, drugname, zip, form):
     output = {'dosage':entries[0], 'qty':entries[1]}
     print(output)
     return output
-    
 
+def get_prices(url, drugname, zip, form, dose, qty):
+    #Load the page
+    updated_url = f'{url}/get-discount?drugName={drugname}&zip={zip}'
+    driver.get(updated_url)
+    #Wait for the page to load
+    page = WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CLASS_NAME,'c-results__item')))
+    #Find the drug FORM selector and click
+    filters = driver.find_elements(By.CLASS_NAME, 'ant-select-selection-item')
+    filters[0].click()
+    #Select the drug form from the dropdown list and wait for it to load
+    form_selection = driver.find_element(By.XPATH , f'//div[@class="ant-select-item-option-content" and text()="{form}"]')
+    WebDriverWait(driver, timeout=10).until(EC.visibility_of(form_selection))
+    driver.save_screenshot('4.png')
+    form_selection.click()
+    page = WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CLASS_NAME,'c-results__item')))
+    #Click on each dropdown menu to update the dropdown list
+    for menu in filters:
+        menu.click()
+    lists = driver.find_elements(By.CLASS_NAME, 'rc-virtual-list-holder-inner')
+    #Click on the drug dose selector and select the provided dose
+    driver.save_screenshot('1.png')
+    filters[1].click()
+    dose_selection = lists[1].find_element(By.XPATH , f'//div[@class="ant-select-item-option-content" and text()="{dose}"]')
+    WebDriverWait(driver, timeout=10).until(EC.visibility_of(dose_selection))
+    driver.save_screenshot('2.png')
+    dose_selection.click()
+    #Click on the qty select and select the provided qty
+    filters[2].click()
+    qty_selection = lists[1].find_element(By.XPATH , f'//div[@class="ant-select-item-option-content" and text()="{qty}"]')
+    WebDriverWait(driver, timeout=10).until(EC.visibility_of(qty_selection))
+    driver.save_screenshot('3.png')
+    qty_selection.click() 
+    #Find the drug price results once they are loaded
+    results = WebDriverWait(driver, timeout=10).until(lambda d: d.find_elements(by=By.CSS_SELECTOR, value='.c-results__item'))
+    output = [result.text for result in results]
+    print(output)
 
-get_drugnames("https://www.discountdrugnetwork.com", "flutica")
-get_drugforms("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416)
-get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Ointment')
-get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Lotion')
-get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Cream (g)')
-get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'SPRAY SUSP')
+# get_drugnames("https://www.discountdrugnetwork.com", "flutica")
+# get_drugforms("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416)
+# get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Ointment')
+# get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Lotion')
+# get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'Cream (g)')
+# get_doseandqty("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'SPRAY SUSP')
+get_prices("https://www.discountdrugnetwork.com", "FLUTICASONE PROPIONATE", 11416, 'SPRAY SUSP', '50 MCG', '16')
